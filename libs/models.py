1fe82ee9ed779afa66850e3444c5d3cebdb0ff67
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-06-22 15:02:22 +0200

    Update models.py

--- libs/models.py
+++ libs/models.py
@@ -1,64 +1,48 @@
 import numpy as np
-import matplotlib.pyplot as plt
-from scipy.optimize import minimize
+from scipy.integrate import odeint
 
-
-def euler_explicite1(f, tinit, Tfinal, y0, h):
-    N = int((Tfinal - tinit) / h)  # ou floor((Tfinal-tinit)/h)
-    Y = np.array([[0, 0, 0]] * (N + 1))  # Tableau numpy avec que des zéros
-    t = tinit + h * np.arange(
-        N + 1)  # Tableau numpy contenant les entiers de 0 à N, t = (tinit, tinit + h, tinit + 2h, ...)
-    Y[0] = y0
-    for n in range(N):
-        Y[n + 1] = Y[n] + h * f(t[n], Y[n])
-    return Y
-
-
-def f_SIR(F, beta, gamma):
-    return np.array([-beta * F[0] * F[1], (beta * F[0] * F[1]) - (gamma * F[1]), gamma * F[1]])
-
-
-def f_SEIR(F, beta, gamma, sigma):
-    return np.array(
-        [-beta * F[0] * F[2], (beta * F[0] * F[2]) - (sigma * F[1]), (sigma * F[1]) - (gamma * F[2]), gamma * F[2]])
-
+### Modèle logistique ###
 
 def logistique(C0, K, r, t):
-    C = np.zeros((len(r), len(t)))
-    for indice in range(len(r)):
-        for d in range(len(t)):
-            C[indice][d] = (K * C0) / (C0 + (K - C0) * np.exp(-r[indice] * t[d]))
+    C = np.zeros((len(t)))
+    for d in range(len(t)):
+        C[d] = (K * C0) / (C0 + (K - C0) * np.exp(-r * t[d]))
     return C
 
+
+### Modèle de Richards ###
 
 def richards(C0, K, r, t, alpha):
-    C = np.zeros((len(r), len(t)))
-    for indice in range(len(r)):
-        for d in range(len(t)):
-            C[indice][d] = (K * C0) / (
-                    C0 ** alpha + (K ** alpha - C0 ** alpha) * np.exp((-r[indice] * t[d] * (K ** alpha)) / (
-                    K ** alpha - C0 ** alpha)) ** (1 / alpha))
+    C = np.zeros((len(t)))
+    for d in range(len(t)):
+        C[d] = (K * C0) / (
+                C0 ** alpha + (K ** alpha - C0 ** alpha) * np.exp(
+            (-r * t[d] * (K ** alpha)) / (K ** alpha - C0 ** alpha)) ** (1 / alpha))
     return C
 
 
-def SIR(beta, gamma, t, start):
-    Y = np.array([[0, 0, 0]] * (len(t)))  # Tableau numpy avec que des zéros
-    Y[0] = start
-    h = t[1] - t[0]
-    for n in range(len(t) - 1):
-        Y[n + 1] = Y[n] + h * f_SIR(Y[n], beta, gamma)
-    return Y
+### Modèle SIR ###
+
+# Les équations différentielles du modèle SIR.
+def deriv_SIR(y, t, N, beta, gamma):
+    S, I, R = y
+    dSdt = -beta * S * I / N
+    dIdt = beta * S * I / N - gamma * I
+    dRdt = gamma * I
+    return dSdt, dIdt, dRdt
 
 
-def SEIR(beta, gamma, sigma, t, start):
-    Y = np.array([[0, 0, 0, 0]] * (len(t)))  # Tableau numpy avec que des zéros
-    Y[0] = start
-    h = t[1] - t[0]
-    for n in range(len(t) - 1):
-        Y[n + 1] = Y[n] + h * f_SEIR(Y[n], beta, gamma, sigma)
-    return Y
+def SIR(S_init, I_init, R_init, t, N_tot, beta, gamma):
+    # Vecteur des conditions initiales.
+    y0 = S_init, I_init, R_init
 
+    # Intégration des équations du modèle SIR.
+    ret = odeint(deriv_SIR, y0, t, args=(N_tot, beta, gamma))
+    I = ret.T[1]
 
-# def mini(f, reel):
-#     return minimize(f, reel)
-
+    # Nombre cumulé d'infectés.
+    C = np.zeros(len(I))
+    C[0] = I[0]
+    for i in range(1, len(I)):
+        C[i] = C[i - 1] + I[i]
+    return C
