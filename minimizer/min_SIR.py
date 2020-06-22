1b14e9d6f26c9815438130f2463a6e8d774839d6
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-06-22 16:02:28 +0200

    Update min_SIR.py

--- minimizer/min_SIR.py
+++ minimizer/min_SIR.py
@@ -9,7 +9,7 @@ import data as dt
 ### Fonctions ###
 
 # Les équations différentielles du modèle SIR.
-def deriv(y, t, N, beta, gamma):
+def deriv_SIR(y, t, N, beta, gamma):
     S, I, R = y
     dSdt = -beta * S * I / N
     dIdt = beta * S * I / N - gamma * I
@@ -18,7 +18,7 @@ def deriv(y, t, N, beta, gamma):
 
 def minimisation_SIR(coeff, observations, init_cond, N):
     t = np.arange(0, len(observations))
-    ret = odeint(deriv, init_cond, t, args=(N, coeff[0], coeff[1]))
+    ret = odeint(deriv_SIR, init_cond, t, args=(N, coeff[0], coeff[1]))
     S, I, R = ret.T
     C = np.zeros(len(I))
     C[0] = I[0]
