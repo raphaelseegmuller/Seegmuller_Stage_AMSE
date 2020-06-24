c2655e519e316e5d9408df6e099dfb8b7fc84ba9
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-06-24 08:55:04 +0200

    Delete fct_erreur.py

--- libs/fct_erreur.py
+++
@@ -1,6 +0,0 @@
-def erreur(y_obs, y_pred):
-    assert len(y_obs) == len(y_pred)
-    res = 0
-    for t_value in range(len(y_obs)):
-        res += (y_obs[t_value] - y_pred[t_value]) ** 2
-    return res
