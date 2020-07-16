ad3f1063c0ce9b9902211ae282a3fa2ecc79cb1a
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-07-16 16:24:17 +0200

    Update min_SIR.py

--- minimizer/min_SIR.py
+++ minimizer/min_SIR.py
@@ -1,12 +1,16 @@
 from libs.useful_fcts import SIR_minimization
 import numpy as np
 
+'''
+Pour lancer ce programme, le sortir du dossier minimizer.
+'''
+
 ### Paramètres ###
 
 # Pays
 country = "France"
 
 # Coefficients
-beta_SIR, gamma_SIR = 10.26658877, 10.1508766
+beta_SIR, gamma_SIR = 43.01327559046377, 42.96070458702876
 
 print("Coefficients optimaux = ", SIR_minimization(np.array([beta_SIR, gamma_SIR]), country))
