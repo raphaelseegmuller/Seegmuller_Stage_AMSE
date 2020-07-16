513b9eec175e85256f4bfc20499b4ae359553d1a
raphaelseegmuller <46761917+raphaelseegmuller@users.noreply.github.com>
2020-07-16 16:24:36 +0200

    Update min_SEIR.py

--- minimizer/min_SEIR.py
+++ minimizer/min_SEIR.py
@@ -1,12 +1,17 @@
 from libs.useful_fcts import SEIR_minimization
 import numpy as np
 
+'''
+Pour lancer ce programme, le sortir du dossier minimizer.
+'''
+
 ### Paramètres ###
 
 # Pays
 country = "France"
 
 # Coefficients
-beta_SEIR, sigma_SEIR, gamma_SEIR = 43.67699061, 3.51088774, 41.67800878
+# beta_SEIR, sigma_SEIR, gamma_SEIR = 45.70046793, 3.4581713, 45.00777174
+beta_SEIR, sigma_SEIR, gamma_SEIR = 397.86333590847744, 49.17696647366713, 397.3828531005903
 
 print("Coefficients optimaux = ", SEIR_minimization(np.array([beta_SEIR, sigma_SEIR, gamma_SEIR]), country))
